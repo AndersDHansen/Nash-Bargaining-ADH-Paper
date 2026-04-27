8ff3b86e75d469f199ef648653371f7eaaea1701
marco-saretta-DTU <mcsr@dtu.dk>
2026-04-27 11:04:00 +0200

    Fix typo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import ppa_symmetric_info
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg:DictConfig):
     print("Hello from nash-bargaining-adh-paper!")
-    print(cfg.config)
+    print(cfg)
 
 if __name__ == "__main__":
     main()

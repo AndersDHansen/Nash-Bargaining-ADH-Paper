df7ebdccb4296670e7c26a9857c96b7d496821d4
marco-saretta-DTU <mcsr@dtu.dk>
2026-04-29 17:48:14 +0200

    Add runner file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
-import ppa_symmetric_info
+from ppa_symmetric_info.runner import Runner
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg:DictConfig):
+    runner = Runner(cfg)
+    print(runner.config)
     print("Hello from nash-bargaining-adh-paper!")
-    print(cfg)
 
 if __name__ == "__main__":
     main()
---
+++ ppa_symmetric_info/runner/runner.py
@@ -0,0 +1,5 @@
+
+class Runner:
+    
+    def __init__(self, config):
+        self.config = config
